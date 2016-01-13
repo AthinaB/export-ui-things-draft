cd0528a90db789782b66eee19a1958776b8a2fee
Athina Bekakou <athinabek@grnet.gr>
2016-01-13 23:01:31 +0200

    Add scripts to extract owner and usage of project

--- projs_data.js
+++ projs_data.js
@@ -37,3 +37,28 @@ $('#table-items-total tr').each(function(i) {
 	// The change of line is usefull to paste the names in excel column aftrewards 
 	$scr_out.append(date + '<br>');
 });
+
+// Get total usage of projects
+$('#table-items-total tr').each(function(i) {
+	var proj_usage = $(this).find('td:eq(-1) dd:eq(-1)').text();
+	console.log(proj_usage)
+	// The change of line is usefull to paste the names in excel column aftrewards 
+	$scr_out.append(proj_usage + '<br>');
+});
+
+
+
+// Get the owners of the projects and put them in the above element
+$('#table-items-total tr').each(function(i) {
+	var $proj_owner_td = $(this).find('td:eq(2)');
+	var owner_name = '';
+	if($proj_owner_td.children('span').length > 0) {
+		owner_name = $proj_owner_td.find('span span').attr('data-content');
+	}
+	else {
+		owner_name = $proj_owner_td.html();
+	}
+	console.log(owner_name)
+	// The change of line is usefull to paste the names in excel column aftrewards 
+	$scr_out.append(owner_name + '<br>');
+});
